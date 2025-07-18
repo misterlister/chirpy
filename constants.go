336adb786e002043c981d634c6d17a2a99d69fe3
misterlister <haydenglister@gmail.com>
2025-07-18 15:37:34 -0700

    Moved constants to a separate file

---
+++ constants.go
@@ -0,0 +1,18 @@
+package main
+
+const (
+	Port        = "8080"
+	AppPrefix   = "/app"
+	ApiPrefix   = "/api"
+	AdminPrefix = "/admin"
+	HealthPath  = "/healthz"
+	MetricPath  = "/metrics"
+	ResetPath   = "/reset"
+	GetReq      = "GET "
+	PutReq      = "PUT "
+	DeleteReq   = "DELETE "
+	PostReq     = "POST "
+	TextHeader  = "text/plain; charset=utf-8"
+	HtmlHeader  = "text/html"
+	ContentType = "Content-Type"
+)
