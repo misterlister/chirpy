8fbc3db6fa8a1b4bcf6dfcb7412677e0ab81d954
misterlister <haydenglister@gmail.com>
2025-08-02 11:23:50 -0700

    Added database queries connection to main

--- main.go
+++ main.go
@@ -1,12 +1,29 @@
 package main
 
 import (
+	"database/sql"
 	"log"
 	"net/http"
+	"os"
+
+	"github.com/joho/godotenv"
+	_ "github.com/lib/pq"
+	"github.com/misterlister/chirpy/internal/database"
 )
 
 func main() {
-	var apiCfg apiConfig
+	godotenv.Load()
+	dbURL := os.Getenv("DB_URL")
+	db, err := sql.Open("postgres", dbURL)
+	if err != nil {
+		log.Println("Error creating database connection")
+		return
+	}
+	dbQueries := database.New(db)
+	apiCfg := apiConfig{
+		dbQueries: dbQueries,
+	}
+
 	serveMux := http.NewServeMux()
 	server := &http.Server{
 		Addr:    ":" + Port,
