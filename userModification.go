5dbc533eb838fc9f8668da8f61081f3f98a11c26
misterlister <haydenglister@gmail.com>
2025-08-05 15:22:23 -0700

    Added function for creating users

---
+++ userModification.go
@@ -0,0 +1,35 @@
+package main
+
+import (
+	"net/http"
+)
+
+func (cfg *apiConfig) handlerUserCreate(w http.ResponseWriter, req *http.Request) {
+	params, err := decodeParams(req)
+
+	if err != nil {
+		respondWithError(w, 400, UnknownErrMsg)
+		return
+	}
+
+	if !(len(params.Email) > 0) {
+		respondWithError(w, 400, MissingParamErrMsg)
+		return
+	}
+
+	user, err := cfg.dbQueries.CreateUser(req.Context(), params.Email)
+
+	if err != nil {
+		respondWithError(w, 500, DatabaseErrMsg+": "+err.Error())
+		return
+	}
+
+	userObj := User{
+		ID:        user.ID,
+		CreatedAt: user.CreatedAt,
+		UpdatedAt: user.UpdatedAt,
+		Email:     user.Email,
+	}
+
+	respondWithJSON(w, 201, userObj)
+}
