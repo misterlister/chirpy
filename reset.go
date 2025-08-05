4fa52763f29c5e13e205d3313e91c1b8eab9c1fe
misterlister <haydenglister@gmail.com>
2025-08-05 15:21:53 -0700

    Added deleting users fuctionality to admin reset function

--- reset.go
+++ reset.go
@@ -3,7 +3,19 @@ package main
 import "net/http"
 
 func (cfg *apiConfig) handlerReset(w http.ResponseWriter, req *http.Request) {
+	if cfg.platform != "dev" {
+		respondWithError(w, 403, UnauthorizedErrMsg)
+		return
+	}
+
+	err := cfg.dbQueries.DeleteAllUsers(req.Context())
+
+	if err != nil {
+		respondWithError(w, 500, DatabaseErrMsg)
+		return
+	}
+
 	cfg.fileserverHits.Store(0)
 	w.WriteHeader(http.StatusOK)
-	w.Write([]byte("Hits reset to 0"))
+	w.Write([]byte(ResetMsg))
 }
