3cfa8cab14952332fe23329ccb7836a797e79df7
misterlister <haydenglister@gmail.com>
2025-08-07 16:14:23 -0700

    Added password to user creation

--- userModification.go
+++ userModification.go
@@ -3,6 +3,9 @@ package main
 import (
 	"encoding/json"
 	"net/http"
+
+	"github.com/misterlister/chirpy/internal/auth"
+	"github.com/misterlister/chirpy/internal/database"
 )
 
 func (cfg *apiConfig) handlerUserCreate(w http.ResponseWriter, req *http.Request) {
@@ -20,7 +23,19 @@ func (cfg *apiConfig) handlerUserCreate(w http.ResponseWriter, req *http.Request
 		return
 	}
 
-	user, err := cfg.dbQueries.CreateUser(req.Context(), params.Email)
+	hashedPassword, err := auth.HashPassword(params.Password)
+
+	if err != nil {
+		respondWithError(w, 500, err.Error())
+		return
+	}
+
+	userParams := database.CreateUserParams{
+		Email:          params.Email,
+		HashedPassword: hashedPassword,
+	}
+
+	user, err := cfg.dbQueries.CreateUser(req.Context(), userParams)
 
 	if err != nil {
 		respondWithError(w, 500, DatabaseErrMsg+": "+err.Error())
