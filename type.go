4ea048d7ec898806ad2f3a25c920ceea9f4f3c92
misterlister <haydenglister@gmail.com>
2025-08-08 22:06:46 -0700

    Added login response struct

--- type.go
+++ type.go
@@ -12,14 +12,15 @@ type apiConfig struct {
 	fileserverHits atomic.Int32
 	dbQueries      *database.Queries
 	platform       string
+	secret         string
 }
 
-type create_user_parameters struct {
+type createUserParameters struct {
 	Email    string `json:"email"`
 	Password string `json:"password"`
 }
 
-type create_chirp_parameters struct {
+type createChirpParameters struct {
 	Body   string    `json:"body"`
 	UserID uuid.UUID `json:"user_id"`
 }
@@ -28,9 +29,10 @@ type errorMessage struct {
 	Error string `json:"error"`
 }
 
-type login_parameters struct {
-	Password string `json:"password"`
-	Email    string `json:"email"`
+type loginParameters struct {
+	Password         string `json:"password"`
+	Email            string `json:"email"`
+	ExpiresInSeconds *int   `json:"expires_in_seconds,omitempty"`
 }
 
 type User struct {
@@ -46,4 +48,12 @@ type Chirp struct {
 	UpdatedAt time.Time `json:"updated_at"`
 	Body      string    `json:"body"`
 	UserID    uuid.UUID `json:"user_id"`
+}
+
+type loginResponse struct {
+	ID        uuid.UUID `json:"id"`
+	CreatedAt time.Time `json:"created_at"`
+	UpdatedAt time.Time `json:"updated_at"`
+	Email     string    `json:"email"`
+	Token     string    `json:"token"`
 }
